7f057e1a70c3ef7e8e77e15eb867968c5d55b0ef
liviuba <anita.liviu@gmail.com>
2015-05-02 20:02:08 +0300

    Sorted out how you pick contacts

--- superMApp/src/proiectMDS/superMApp/superMApp.java
+++ superMApp/src/proiectMDS/superMApp/superMApp.java
@@ -2,14 +2,49 @@ package proiectMDS.superMApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.util.Log;
+
 
 public class superMApp extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+			final int PICK_CONTACT = 1;
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.main);
+
+			Button button = (Button) findViewById(R.id.asdf);
+			button.setOnClickListener(new View.OnClickListener(){
+				@Override
+				public void onClick(View v){
+					Log.e("superMApp:OnClick","Intru in onCLick!");
+					Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+					startActivityForResult( pickContactIntent, PICK_CONTACT);
+				}
+			});
     }
+
+		public void test(View view){
+			final int PICK_CONTACT = 1;
+			Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+			startActivityForResult( pickContactIntent, PICK_CONTACT);
+		}
+
+		@Override
+		public void onActivityResult( int requestCode, int resultCode, Intent data){
+			super.onActivityResult(requestCode, resultCode, data);
+
+			if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+				Uri contactData = data.getData();
+				Log.e("superMApp:onActivityResult", contactData.toString());
+			}
+
+		}
 }
