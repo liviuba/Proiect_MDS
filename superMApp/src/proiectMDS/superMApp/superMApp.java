d5c4481b0888815f3e69f661365b38e7c59d5701
liviuba <anita.liviu@gmail.com>
2015-05-03 23:07:48 +0300

    Got contact info

--- superMApp/src/proiectMDS/superMApp/superMApp.java
+++ superMApp/src/proiectMDS/superMApp/superMApp.java
@@ -9,42 +9,67 @@ import android.content.Intent;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.database.Cursor;
 
 
 public class superMApp extends Activity
 {
+		final int PICK_CONTACT = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-			final int PICK_CONTACT = 1;
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.main);
 
-			Button button = (Button) findViewById(R.id.asdf);
+			Button button = (Button) findViewById(R.id.pick_contact_b);
 			button.setOnClickListener(new View.OnClickListener(){
 				@Override
 				public void onClick(View v){
-					Log.e("superMApp:OnClick","Intru in onCLick!");
+					Log.e("superMApp:OnClick","Picking a contact");
 					Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
 					startActivityForResult( pickContactIntent, PICK_CONTACT);
 				}
 			});
     }
 
-		public void test(View view){
-			final int PICK_CONTACT = 1;
-			Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-			startActivityForResult( pickContactIntent, PICK_CONTACT);
-		}
-
 		@Override
 		public void onActivityResult( int requestCode, int resultCode, Intent data){
 			super.onActivityResult(requestCode, resultCode, data);
 
-			if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+			//TODO switch-if
+			if(requestCode == PICK_CONTACT && resultCode == Activity.RESULT_OK){
 				Uri contactData = data.getData();
-				Log.e("superMApp:onActivityResult", contactData.toString());
-			}
+				Cursor c = managedQuery( contactData, null, null, null, null );
 
+				//Log.e("ASDFTEST", (String) c.moveToFirst());
+				if( c.moveToFirst() ){
+					String contactName = c.getString( c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME) );
+					String contactPhoneNum;
+
+					/** Get phone # (if any) **/
+					String id = c.getString( c.getColumnIndexOrThrow( ContactsContract.Contacts._ID ));
+					String hasPhone = c.getString( c.getColumnIndex( ContactsContract.Contacts.HAS_PHONE_NUMBER ) );
+					
+					if(hasPhone.equalsIgnoreCase("1")){
+						Cursor phones = getContentResolver().query(
+								ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+								ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = "+id,
+								null, null);
+						phones.moveToFirst();
+
+						contactPhoneNum = phones.getString(phones.getColumnIndex("data1"));
+					}
+					else{
+						contactPhoneNum = "No phone number. Run, Forest, run!!";
+					}
+					Log.e("superMApp:ASDFTEST", contactName + "--" + contactPhoneNum);
+
+				}
+				else{
+					Log.e("superMApp:onActivityResult","No first entry for contact DB cursor");
+				}
+
+			}
 		}
 }
