7569cd1c40173987883cca2b65ea750654550b26
andreipurdila <andrei.purdila@gmail.com>
2015-05-25 01:46:15 +0300

    added some checking

--- superMApp/app/src/main/java/proiectMDS/superMApp/SmsListener.java
+++ superMApp/app/src/main/java/proiectMDS/superMApp/SmsListener.java
@@ -3,18 +3,11 @@ package proiectMDS.superMApp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -31,18 +24,23 @@ public class SmsListener extends BroadcastReceiver {
     private static final String alertToken = "trei";
     private static String message = null;
     private static String phoneNumber = null;
+    private static double lat;
+    private static double lon;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
 			/********************TEST **********************/
-				final String TRACKED_FILENAME = "myTracked";
-				SharedPreferences tracked = null;
+        final String TRACKED_FILENAME = "myTracked";
+        SharedPreferences tracked = null;
 
-				tracked = context.getSharedPreferences(TRACKED_FILENAME, 0);
+        tracked = context.getSharedPreferences(TRACKED_FILENAME, 0);
 
-				int nr = tracked.getInt("TRACKED_NUM", -100);
-				Log.e("ASDFTOMATO", Integer.toString(nr));
+        int nr = tracked.getInt("TRACKED_NUM", -100);
+        Log.e("ASDFTOMATO", Integer.toString(nr));
+
+        for(int i=0; i<nr; i++)
+            Log.e("ASDFTOMATO", tracked.getString( Integer.toString(i), "Couldn't retrieve tracked") );
 
 
 
@@ -70,14 +68,12 @@ public class SmsListener extends BroadcastReceiver {
                 // start the alarm through a Service if the sms is a notification from
                 //someone who is in danger and send the coordinates to GoogleMaps
 
-
-								//FIXME: tests. Replace w/ previous when persistent storage gets done
+                //FIXME: tests. Replace w/ previous when persistent storage gets done
                 if (someoneInDanger() && notAJoke()) {
 									if (true) {
 											//Extract the coordinates and send them
 											// to the Maps Activity
-											String[] coordinates = message.split(",",2);
-											LatLng position = new LatLng(Double.parseDouble(coordinates[0]), Double.parseDouble(coordinates[1]));
+											LatLng position = new LatLng(lat, lon);
 											Bundle arg = new Bundle();
 											arg.putParcelable("position", position);
 											Intent acIntent = new Intent(context,MapsActivity.class);
@@ -107,19 +103,43 @@ public class SmsListener extends BroadcastReceiver {
     private boolean notAJoke(){
         //search for the originating phone number
         // in the tracked list
-        for (String str : superMApp.trackedList)
-            if (str.endsWith(phoneNumber))
-                return true;
+//        for (String str : superMApp.trackedList)
+//            if (str.endsWith(phoneNumber))
+//                return true;
+
+
+
+
         return true;	//false
     }
+    
 
     private boolean someoneInDanger(){
         //if message starts with the token, then
-        //delete the token and return true;
-        if (message.startsWith(alertToken)){
-            message = message.substring(alertToken.length() );
-            return true;
+        //delete the token
+
+        String[] parts = message.split(",");
+        for (String str : parts)
+            Log.i("someoneInDanger():", str);
+        if (parts.length <= 4)
+            return false;
+
+        if (!parts[0].equals(alertToken))
+            return false;
+
+
+        try {
+            lat = Double.parseDouble(parts[1]);
+            lon = Double.parseDouble(parts[2]);
         }
-        return false;	//false
+        catch (Exception e){
+            return false;
+        }
+
+        if (!parts[3].startsWith("https://www.google.com/maps/@"))
+            return false;
+
+        return true;
+
     }
 }
